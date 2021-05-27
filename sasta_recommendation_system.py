0ccc9d22cf2db0eb3da9950da2fa41b0a31fb222
Siva Charan N <71810806+Charan-N@users.noreply.github.com>
2021-05-27 23:09:43 +0530

    Create sasta_recommendation_system.py

---
+++ sasta_recommendation_system.py
@@ -0,0 +1,9 @@
+import random
+
+#Change this list by adding your movies of choice and let the algorithm decide for you!! :)
+movies = ['Captain Fantastic', 'WW84', 'The Social Network', 'Logan', 'Padhle BSDK!'] #the last one is if you should be studying instead of watching movies
+                                                                                      #now don't blame the algorithm if it suggests you to watch movie in that case!
+selection = random.choice(movies)
+
+print("Sasta Recommendation System suggests that you should go for: ", selection)
+#Lets see if Netflix and YouTube can beat this!!
